c20485f09eb479c8dfeb93b0c13fb24b5a756a6c
zahid <rashidzahid819@gmail.com>
2023-05-23 01:28:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ shareButton.addEventListener('click', event =>{
     navigator.share({
       title: "Find Your Shaar",
       text: 'Yayy!! I found my Shaar. You can too at this link!',
+      text: urduPoetries[xookMe],
       url:"https://visionary-chebakia-c1f2a5.netlify.app/",
     }).then(() => {
       console.log("Thanks");
